af9dbea930fcc56f07bf49171fc34f99e1f21595
Hakob_Hakobyan3 <hakob.hakobyan.-nd@disney.com>
2022-08-29 16:12:02 +0400

    Initial commit

---
+++ src/test/java/com/linkedinlearning/reactivespring/ReactivespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedinlearning.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReactivespringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
